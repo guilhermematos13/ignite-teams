f65cc7cf4cda853a76bbc4b2abfceb57e2603568
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-09 15:27:30 -0300

    feat: config expo google fonts and other libraries

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
-import Groups from "./src/screens/Groups";
+import Groups from "@screens/Groups";
+import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components/native";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <>
-      <Groups />
-    </>
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
