1d95e34983ea53dd00f0f117881140e20a961ebf
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-09 01:46:08 -0300

    feat: config babel plugins

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Groups from "./src/Groups";
+import Groups from "./src/screens/Groups";
 
 export default function App() {
   return (
