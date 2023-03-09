8ede113cf548c5ab33c787df59d78434007ce471
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-09 01:41:11 -0300

    create project and config alias for folders

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Groups from "./src/Groups";
+
+export default function App() {
+  return (
+    <>
+      <Groups />
+    </>
+  );
+}
