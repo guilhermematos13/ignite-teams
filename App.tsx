7fdf0e72644ebb01f813951a83a40e9604b86781
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-04-03 17:37:08 -0300

    feat: create input component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import Groups from "@screens/Groups";
 import theme from "./src/theme";
 import {
   useFonts,
@@ -8,6 +7,7 @@ import {
 import { ThemeProvider } from "styled-components/native";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
