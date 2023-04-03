477978a9e9d3bd6f3650725b65f7a5f742b8b539
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-04-03 15:20:28 -0300

    feat: create list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,27 @@
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma ?" />
+        )}
+      />
     </Container>
   );
 }
