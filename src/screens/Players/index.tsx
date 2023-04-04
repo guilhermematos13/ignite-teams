ea437dab7a8f5d092acc6fe8a0e4e57f49982244
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-04-04 00:58:49 -0300

    feat: create button icon and filter teams components

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,15 @@
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -11,6 +17,25 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input placeholder="Nome do participante" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
