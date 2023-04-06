5b38fd693518ca09221ac50444471d01def455a1
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-04-06 14:49:02 -0300

    feat: completion of the route part

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "@components/Header";
@@ -7,10 +8,11 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { group: "Players" });
+    navigation.navigate("players", { group });
   }
 
   return (
@@ -23,7 +25,7 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle="crie uma turma para adicionar pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
